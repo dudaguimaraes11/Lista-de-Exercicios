88524528686555d1e4f4ac7b1f8d4a758283aa4c
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:56:28 -0300

    Filtra os elementos do array

---
+++ atividade14.js
@@ -0,0 +1,13 @@
+// 14 – Destaques da semana no blog 
+let artigosBlog = [
+  "Como usar Docker com Node.js",
+  "5 erros comuns em JavaScript",
+  "Introdução ao TypeScript",
+  "Clean Code na prática",
+  "Design Patterns úteis",
+  "Refatoração de código",
+  "Boas práticas de API REST",
+  "Segurança em aplicações web"
+];
+
+console.log(`4 artigos mais recentes da semana: ${artigosBlog.slice(4, 6)}`); 
