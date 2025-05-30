60dc4594b8af814f659d000c30863ea7c050545a
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:43:15 -0300

    Converte o array em uma string

---
+++ atividade12.js
@@ -0,0 +1,12 @@
+// 12 – Tags para URL
+
+let palavrasChave = [
+  "desenvolvimento",
+  "frontend",
+  "javascript",
+  "css",
+  "react"
+];
+
+
+console.log(`Palavras Chave: ${palavrasChave.join(', ')}`);
