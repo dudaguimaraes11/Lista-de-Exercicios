b04f85b8f28a3454027ade11a1623d4d18bc8754
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:24:44 -0300

    Adiciona elementos no início do array

---
+++ atividade08.js
@@ -0,0 +1,14 @@
+// 8 – Alunos transferidos
+let chamada = [
+  "Carlos",
+  "Fernanda",
+  "João",
+  "Mariana",
+  "Lucas",
+  "Camila"
+];
+
+ 
+chamada.unshift('Beatriz', 'Rafaela');
+
+console.log("Fila de atendimento atualizada:", chamada);
