1023da193efd6eff5dca9e54f6921c7593f4a6da
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:26:52 -0300

    Verficação na lista de array

---
+++ atividade09.js
@@ -0,0 +1,11 @@
+9 – Conferência de presença
+
+let presenca = [
+  "João Lima",
+  "Carla Nunes",
+  "Amanda Souza",
+  "Bruno Ribeiro",
+  "Fernanda Sales"
+];
+
+console.log(`A lista de prensença inclui Amanda Souza? ${presenca.includes("Amanda Souza")}`);
