13e2537068dd9186dc2439a1ecf30f0072403db4
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:22:14 -0300

    Adiciona elementos no início do array

---
+++ atividade07.js
@@ -0,0 +1,15 @@
+//  7 – Prioridade na fila
+
+let filaAtendimento = [
+  "cliente 003",
+  "cliente 004",
+  "cliente 005",
+  "cliente 006",
+  "cliente 007"
+];
+
+
+ 
+filaAtendimento.unshift('Cliente 001', 'Cliente 002');
+
+console.log("Fila de atendimento atualizada:", filaAtendimento);
