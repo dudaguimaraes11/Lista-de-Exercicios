248f931e8956acf32cbda09041d1de223f83611e
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:51:54 -0300

    Filtra os elementos do array

---
+++ atividade13.js
@@ -0,0 +1,14 @@
+// 13 – Relatório de férias: Primeira semana
+
+let diasFaltas = [
+  "Carlos – Segunda",
+  "Carlos – Terça",
+  "Carlos – Quarta",
+  "Carlos – Quinta",
+  "Carlos – Sexta",
+  "Carlos – Segunda (semana 2)",
+  "Carlos – Terça (semana 2)",
+  "Carlos – Quarta (semana 2)"
+];
+
+console.log(`Relatório da primeira semana de férias: ${diasFaltas.slice(0, 5)}`);
