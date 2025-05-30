a4634a01a65902a332a8f108fe864cd40773cb9a
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:07:47 -0300

    Remove produtos com defeito do array

---
+++ atividade03.js
@@ -0,0 +1,15 @@
+// 3 – Último produto com defeito
+let producao = [
+  "mousepad",
+  "placa de vídeo",
+  "fonte de alimentação",
+  "HD externo",
+  "notebook gamer",
+  "roteador"
+];
+
+let produtoRemovido = producao.pop();
+
+console.log("Lista de produtos depois de remover o produto com defeito:", producao);
+console.log("Produto com defeito removido:", produtoRemovido); 
+
