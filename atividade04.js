5f7329d601f5e5be930424fcb026fcc3ce91faa2
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:12:10 -0300

    Remove o último elemento do array

---
+++ atividade04.js
@@ -0,0 +1,11 @@
+// 4 – Cancelamento de última inscrição
+
+let inscritos = [
+  "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
+];
+
+let inscricaoRemovida = inscritos.pop();
+
+console.log("Após a remoção do último integrante a se inscrever", inscritos);
+console.log("Inscrição removida:", inscricaoRemovida); 
+
