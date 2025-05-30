845431f135f4476f0a99845e98d8932f96e52c21
Maria Eduarda <maria.e.guimaraes19@aluno.senai.br>
2025-05-30 11:38:40 -0300

    Verifica elemento do array

---
+++ atividade10.js
@@ -0,0 +1,12 @@
+// 10 – Verificação de produto em estoque
+
+let estoque = [
+  "mesa para PC",
+  "cadeira gamer",
+  "monitor 27 polegadas",
+  "hub USB",
+  "mouse sem fio"
+];
+
+
+console.log(`A lista de prensença inclui cadeira gamer? ${estoque.includes("cadeira gamer")}`); 
