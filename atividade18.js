26704b29e62835f75638500912681d7c38f91ab2
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-06-08 22:59:49 -0300

    Feat:Unifica os arrays Hardware e Periféricos

---
+++ atividade18.js
@@ -0,0 +1,17 @@
+// 18 – Catálogo unificado
+
+let hardware = [
+  "processador i7",
+  "placa-mãe",
+  "memória RAM 16GB",
+  "SSD 1TB"
+];
+
+let perifericos = [
+  "teclado gamer",
+  "mouse ótico",
+  "webcam HD",
+  "fone com microfone"
+];
+
+console.log(`catálogo unificado com os produtos das categorias hardware e periféricos: ${hardware.concat(perifericos)}`);
