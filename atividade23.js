3bfe4c47e40ae4c2b3596a6310bad710af6b5004
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-06-08 23:20:48 -0300

    Feat: Ordena o array  alfabeticamente

---
+++ atividade23.js
@@ -0,0 +1,16 @@
+// 23 – Organizar lista de candidatos
+
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+
+let candidatosOrdenadosAlfabeticamente = candidatos.sort();
+
+console.log("Lista de candidatos ordenada:", candidatosOrdenadosAlfabeticamente);
