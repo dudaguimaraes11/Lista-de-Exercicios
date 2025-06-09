5541e301cc3f0788f993c47e1c193d5f73ea7893
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-06-08 22:39:36 -0300

    Feat: Remove elementos do array

---
+++ atividade15.js
@@ -0,0 +1,16 @@
+//15)  15 – Remoção de itens inativos no menu
+
+let menuNavegacao = [
+  "Início",
+  "Serviços",
+  "Portfólio",
+  "Blog",
+  "Contato",
+  "Login",
+  "Área do cliente"
+];
+
+
+menuNavegacao.splice(1, 2); 
+console.log(`Menu atualizado:`, menuNavegacao);
+
