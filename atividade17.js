6a1b82f2cf98144b585acc2157de6d509363453d
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-06-08 22:57:51 -0300

    Feat: Unifica dois arrays

---
+++ atividade17.js
@@ -0,0 +1,17 @@
+// 17 – Atualização de lista de usuários
+
+let usuariosSistemaA = [
+  "aline.souza",
+  "maria.ferreira",
+  "carlos.lima",
+  "ana.martins"
+];
+
+let usuariosSistemaB = [
+  "joao.silva",
+  "paulo.pereira",
+  "fernanda.alves",
+  "marcelo.cardoso"
+];
+
+console.log(`Atualização da lista de usuários combinada: ${usuariosSistemaA.concat(usuariosSistemaB)}`);
