b675e2b2edaa9b9bf5b753f27af709bd9ea08bd6
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-06-08 23:27:15 -0300

    Feat: Inverte a ordem do array

---
+++ atividade26.js
@@ -0,0 +1,15 @@
+//  26 – Histórico de navegação
+
+let historico = [
+  "login",
+  "painel",
+  "relatórios",
+  "detalhes do cliente",
+  "editar dados",
+  "configurações",
+  "logout"
+];
+
+let historicoInvertido = historico.reverse();
+
+console.log("Histórico invertido:", historicoInvertido);
