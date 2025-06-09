139ef8d631c335d59367aaa2d9bda9d3e35fa515
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-06-08 23:23:34 -0300

    Feat: Ordena de forma decrescente os elementos do array

---
+++ atividade24.js
@@ -0,0 +1,7 @@
+//  24 – Ranking de vendas
+
+let vendas = [3500, 1800, 4200, 3900, 5000, 1200, 2900, 4700];
+
+let vendasOrdenadas = vendas.sort((a, b) => b - a);
+
+console.log("Lista de vendas ordenada:", vendasOrdenadas);
