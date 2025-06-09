56dfb69a22b62fcadc5b73c88a5777458418ee3b
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-06-08 23:25:46 -0300

    Feat: Inverte a ordem do array

---
+++ atividade25.js
@@ -0,0 +1,16 @@
+// 25 – Últimas atualizações do sistema
+
+let atualizacoes = [
+  "Backup diário concluído",
+  "Servidor reiniciado",
+  "Nova versão implantada",
+  "Logs exportados",
+  "Firewall atualizado",
+  "Monitoramento ativado",
+  "Banco de dados otimizado",
+  "Script de segurança executado"
+];
+
+let atualizacoesInvertidas = atualizacoes.reverse();
+
+console.log("Lista de atualizações invertida:", atualizacoesInvertidas);
